5f85748a41646c329476fb0ae1d7133091da0848
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-06-27 13:21:11 +0200

    More tests for edge cases

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -20,6 +20,26 @@ def test_on_page_markdown(plugin):
     assert ('!!! info "Title"\n\tTitled block\n\tTwo lines'
             in plugin.on_page_markdown(test_markdown, None, None, None))
 
+    # Test titles with spaces
+    test_markdown = '> [!INFO] This title has spaces\n> And text'
+    assert ('!!! info "This title has spaces"\n\tAnd text'
+            in plugin.on_page_markdown(test_markdown, None, None, None))
+
+    # Test callout paragraphs (empty callout line)
+    test_markdown = '> [!INFO] Title\n> Paragraph 1\n>\n> Paragraph 2'
+    assert ('!!! info "Title"\n\tParagraph 1\n\t\n\tParagraph 2'
+            in plugin.on_page_markdown(test_markdown, None, None, None))
+
+    # Test > block within callout
+    test_markdown = '> [!INFO] Title\n> > Block within callout'
+    assert ('!!! info "Title"\n\t> Block within callout'
+            in plugin.on_page_markdown(test_markdown, None, None, None))
+
+    # Test > in callout content
+    test_markdown = '> [!INFO] Title\n> The > character'
+    assert ('!!! info "Title"\n\tThe > character'
+            in plugin.on_page_markdown(test_markdown, None, None, None))
+
     # Test folded block, closed by default
     test_markdown = '> [!INFO]- Folded block\n> Folded content'
     assert ('??? info "Folded block"\n\tFolded content'
@@ -54,7 +74,12 @@ def test_on_page_markdown(plugin):
     assert ('!!! custom "Callout"\n\tCallout Text\nSome text\n> Block'
             in plugin.on_page_markdown(test_markdown, None, None, None))
 
-    # Test wikilink
+    # Test wikilink in regular block
     test_markdown = '> [[Wikilink]] in a block\n> [Link](https://example.com)'
     assert ('> [[Wikilink]] in a block\n> [Link](https://example.com)'
+            in plugin.on_page_markdown(test_markdown, None, None, None))
+
+    # Test callouts without whitespace
+    test_markdown = '>[!INFO] Title\n>Content'
+    assert ('!!! info "Title"\n\tContent'
             in plugin.on_page_markdown(test_markdown, None, None, None))
