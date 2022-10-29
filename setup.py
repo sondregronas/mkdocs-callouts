f6a65e5ba5568d5af0c434de9366214ff2bc623b
Sondre Grønås <sondre.gronas@gmail.com>
2022-10-29 23:01:07 +0200

    Add reference to package in setup.py for github stats

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
-from setuptools import setup ; setup()
+from setuptools import setup
+
+raise RuntimeError("Installation using setup.py is not supported, use 'pip install .' instead.")
+
+# Fake reference so GitHub still considers it a real package.
+setup(name="mkdocs-callouts")
