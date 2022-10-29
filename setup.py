187648aeb14d1f72831dc3b6d7e76bca2a5692db
Sondre Grønås <sondre.gronas@gmail.com>
2022-10-29 23:03:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
 
-raise RuntimeError("Installation using setup.py is not supported, use 'pip install .' instead.")
-
 # Fake reference so GitHub still considers it a real package.
 setup(name="mkdocs-callouts")
